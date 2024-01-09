27ec84dc6932a39008afabe08a493398d8cb66e0
Mrkva7 <152974608+Mrkva7@users.noreply.github.com>
2024-01-09 12:30:15 +0100

    Add files via upload

---
+++ Vježba3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vježba3
+{
+
+    
+    class Stablo
+    {
+        bool otpadajulistovi;
+        string vrstaStabla;
+        public bool Otpadajulistovi { get => otpadajulistovi; set => otpadajulistovi = value; }
+        public string VrstaStabla { get => vrstaStabla; set => vrstaStabla = value; }
+
+        public Stablo() { }
+
+        public Stablo(bool otpadajulistovi)
+        {
+            this.otpadajulistovi = otpadajulistovi;
+        }
+
+        public override string ToString()
+        {
+            string  ispis = "Ovo stablo je:";
+            if(otpadajulistovi)
+            {
+                ispis += "listopadno.";
+            }
+            else
+            {
+                ispis += "Zimzeleno.";
+            }
+                return ispis;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stablo stablo1 = new Stablo();
+            Stablo stablo2 = new Stablo(); 
+
+            Console.WriteLine(stablo1.ToString());
+            Console.WriteLine(stablo2.ToString());  
+
+
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
